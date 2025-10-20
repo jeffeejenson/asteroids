3071e68ed8f8b579afaa4d233a3c2dd697b7e783
Jeffee Jenson <jeffee125@gmail.com>
2025-10-20 22:57:16 +0530

    added player movements

--- main.py
+++ main.py
@@ -3,19 +3,30 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    t = pygame.time.Clock()
+    dt = 0
+
+
+    player = Player(SCREEN_WIDTH // 2 , SCREEN_HEIGHT // 2  )
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         
         screen.fill("black")
+        player.draw(screen)
         
         pygame.display.flip()
+        dt = t.tick(60) / 1000
+
 
     
 
